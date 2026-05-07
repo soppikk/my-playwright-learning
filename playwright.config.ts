7768842f0659c637f925906f273779c0f73cc6e8
soppikk <skelaptrishvili@solvd.com>
2026-05-07 12:32:08 +0400

    add exercise test from Automationexercise

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
+
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -30,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+        baseURL: 'https://www.saucedemo.com',   // <-- add this
   },
 
   /* Configure projects for major browsers */
---
+++ tests/list-practice.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test'
+
+test('list handling practice', async ({ page }) => {
+  // Login
+  await page.goto('https://automationexercise.com/', { timeout: 60000 })
+  await page.getByText('Signup / Login').click()
+  await page.locator('[data-qa="login-email"]').fill('testsop@gmail.com')
+  await page.locator('[data-qa="login-password"]').fill('paroli12')
+  await page.getByRole('button', { name: 'Login' }).click()
+
+
+//   await page.getByPlaceholder('Username').fill('standard_user')
+//   await page.getByPlaceholder('Password').fill('secret_sauce')
+//   await page.getByRole('button', { name: 'Login' }).click()
+//   //<button type="submit" data-qa="login-button" class="btn btn-default">Login</button>
+// // <input type="email" data-qa="login-email" placeholder="Email Address" name="email" value="" required=""> 
+//   const products = page.locator('.inventory_item')
+
+//   // Count
+//   console.log('count:', await products.count())
+
+//   // Click 2nd product
+//   await products.nth(1).click()
+
+  // Pause to see result, then close
+  await page.waitForTimeout(2000)
+  await page.close()
+})
